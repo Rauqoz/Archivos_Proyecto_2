64b2ee7af7a68db7320ba64d1c8657ff10b70b2a
Rauqoz <raul4.antonio@yahoo.com>
2021-10-20 22:12:00 -0600

    conexion back con base
    udpate

--- Back/index.js
+++ Back/index.js
@@ -1,17 +1,54 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const oracledb = require('oracledb');
+//ORCL18   172.17.0.2
+//b6f6af56ce3b
+
+const config_db = {
+    user : 'dummy',
+    password : 'dummy',
+    connectString : '172.17.0.2:1521/ORCL18'
+}
+
+require('dotenv').config()
+
 
 app.use(cors())
 
 const port = 5300;
 
 app.get('/', (req,res)=>{
+    async function runTest() {
+        let conn;
+       
+        try {
+          conn = await oracledb.getConnection(config_db);
+       
+          const result = await conn.execute(
+            'select * from \"prueba\"'
+          );
+       
+          console.log(result);
+        } catch (err) {
+          console.log(err);
+        } finally {
+          if (conn) {
+            try {
+              await conn.close();
+            } catch (err) {
+              console.log('err');
+            }
+          }
+        }
+      }
+    runTest()
     res.send({home:"hola mundo"})
 })
 
 
-
-app.listen(port, ()=>{
+app.listen(port, async ()=>{
+    
     console.log("server on");
+    
 })
