1fede59124f20517ff7055d16544fb2baac1110e
Rauqoz <raul4.antonio@yahoo.com>
2021-11-06 19:49:02 -0600

    carga masiva full

--- Back/index.js
+++ Back/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const oracledb = require('oracledb');
+const { AQ_DEQ_WAAQ_MSG_DELIV_MODE_PERSISTENTIT_FOREVER } = require('oracledb');
 //ORCL18   172.17.0.2
 //b6f6af56ce3b
+//values
 
 const config_db = {
     user : 'dummy',
@@ -88,145 +90,297 @@ app.post('/c_puesto_formulario', (req,res)=>{
   res.status(200)
 })
 
-var c_deps = 0,c_pues = 0,c_cate=0,c_requi=0,c_forma=0;
-
+var a_deps = [], a_pues = [], a_cate = [],a_requi = [], a_forma = [];
 
 app.post('/c_carga_masiva',(req,res)=>{
   const data = req.body.carga
-  const array_prueba = [];
-  console.log(data);
-  console.log("------");
   c_departamentos(data)
-  console.log('departamentos',c_deps);
-  console.log('puestos',c_pues);
-  console.log('categorias',c_cate);
-  console.log('requisitos',c_requi);
-  console.log('formatos',c_forma);
-  res.send({data:data})
+
+  recorridos_carga_masiva()
+
+  setTimeout(()=>{
+    recorridos_carga_masiva_2()
+    console.log('enlaces listos');
+  },5000)
+
+  res.send({data:'listo'})
 })
 
-const c_departamentos =(hijo)=>{
+const recorridos_carga_masiva = ()=>{
+  a_deps.forEach(e=>{
+    let nombre = e.nombre[0]
+    let cap_total = e.capital_total[0]
+
+    query_select_insertar(`select * from departamento where nombre = '${nombre}'`,`insert into departamento (nombre,capital_total) values ('${nombre}',${cap_total})`)
+  })
+
+  a_pues.forEach(e=>{
+    let nombre = e.nombre[0]
+    let salario = e.salario[0]
+
+    query_select_insertar(`select * from puesto where nombre = '${nombre}'`,`insert into puesto (nombre,salario) values ('${nombre}',${salario})`)
+  })
+
+  a_cate.forEach(e=>{
+    let nombre = e.nombre[0]
+
+    query_select_insertar(`select * from categoria where nombre = '${nombre}'`,`insert into categoria (nombre) values ('${nombre}')`)
+  })
+
+  a_requi.forEach(e=>{
+    let nombre = e.nombre[0]
+    let tamano = e.tamaño[0]
+    let obliga = e.obligatorio[0]
+
+    query_select_insertar(`select * from requisito where nombre = '${nombre}'`,`insert into requisito (nombre,tamano,obligatorio) values ('${nombre}',${tamano},'${obliga}')`)
+  })
+
+  a_forma.forEach(e=>{
+    let nombre = e.nombre[0]
+
+    query_select_insertar(`select * from formato where nombre = '${nombre}'`,`insert into formato (nombre) values ('${nombre}')`)
+  })
+}
+
+const recorridos_carga_masiva_2 = ()=>{
+  a_pues.forEach(pue=>{
+    pue.padre.forEach(dep=>{
+      query_solo_insertar(`INSERT INTO DEPARTAMENTO_PUESTO VALUES ((SELECT ID_DEPARTAMENTO FROM DEPARTAMENTO d WHERE d.nombre = '${dep.nombre[0]}'), (SELECT ID_PUESTO FROM PUESTO p WHERE p.nombre = '${pue.nombre[0]}'))`)
+    })
+  })
+
+  a_cate.forEach(cate=>{
+    cate.padre.forEach(pue=>{
+      query_solo_insertar(`INSERT INTO PUESTO_CATEGORIA VALUES ((SELECT ID_PUESTO FROM PUESTO p WHERE p.NOMBRE = '${pue.nombre[0]}'),(SELECT ID_CATEGORIA FROM CATEGORIA c WHERE c.NOMBRE = '${cate.nombre[0]}'))`)
+    })
+  })
+
+  a_requi.forEach(requi=>{
+    requi.padre.forEach(pue=>{
+      query_solo_insertar(`INSERT INTO PUESTO_REQUISITO VALUES ((SELECT ID_PUESTO FROM PUESTO p WHERE p.NOMBRE = '${pue.nombre[0]}'),(SELECT ID_REQUISITO FROM REQUISITO r WHERE r.NOMBRE = '${requi.nombre[0]}'))`)
+    })
+  })
+
+  a_forma.forEach(forma=>{
+    forma.padre.forEach(requi=>{
+      query_solo_insertar(`INSERT INTO REQUISITO_FORMATO VALUES ((SELECT ID_REQUISITO FROM REQUISITO r2 WHERE r2.NOMBRE = '${requi.nombre[0]}'),(SELECT ID_FORMATO FROM FORMATO f WHERE f.NOMBRE = '${forma.nombre[0]}'))`)
+    })
+  })
+
+}
+
+const c_departamentos = (hijo,padre)=>{
   if(Array.isArray(hijo)){
     hijo.forEach(e=>{
       if(e.nombre){
-        c_deps+=1
-        console.log('**departamento**');
-        console.log('capital dep:',e.capital_total[0]);
-        console.log('nombre dep:',e.nombre[0]);
+        let nombre = e.nombre[0]
+        let cap_total = e.capital_total[0]
+        //console.log('capital dep:',e.capital_total[0]);
+        //console.log('nombre dep:',e.nombre[0]);
+        let tempo = a_deps.find(el => el.nombre == nombre)
+        if(tempo === undefined){
+          a_deps.push(e)
+        }
+
         if(e.departamentos){
-          c_departamentos(e.departamentos)
+          c_departamentos(e.departamentos,hijo)
         }
         if(e.puestos){
-          c_puestos(e.puestos)
+          c_puestos(e.puestos,hijo)
         }
       }else{
-        c_departamentos(e)
+        c_departamentos(e,hijo)
       }
     })
   }else{
     if(hijo.departamento){
-      c_departamentos(hijo.departamento)
+      c_departamentos(hijo.departamento,hijo)
     }else{
-      c_departamentos(hijo.departamentos)
+      c_departamentos(hijo.departamentos,hijo)
     }
   }
 }
 
-const c_puestos =(hijo)=>{
+const c_puestos =(hijo,padre)=>{
   if(Array.isArray(hijo)){
     hijo.forEach(e=>{
       if(e.nombre){
-        c_pues+=1
-        console.log('**puesto**');
-        console.log('capital pues:',e.salario[0]);
-        console.log('nombre pues:',e.nombre[0]);
+        let nombre = e.nombre[0]
+        let salario = e.salario[0]
+        //console.log('capital pues:',e.salario[0]);
+        //console.log('nombre pues:',e.nombre[0]);
+        let tempo = a_pues.find(el => el.nombre == nombre)
+        if(tempo === undefined){
+          a_pues.push({...e,padre:padre})
+        }
+
         if(e.puestos){
-          c_puestos(e.puestos)
+          c_puestos(e.puestos,padre)
         }
         if(e.categorias){
-          c_categorias(e.categorias)
+          c_categorias(e.categorias,hijo)
         }
         if(e.requisitos){
-          c_requisitos(e.requisitos)
+          c_requisitos(e.requisitos,hijo)
         }
         
       }else{
-        c_puestos(e)
+        c_puestos(e,padre)
       }
     })
   }else{
     if(hijo.puesto){
-      c_puestos(hijo.puesto)
+      c_puestos(hijo.puesto,padre)
     }else{
-      c_puestos(hijo.puestos)
+      c_puestos(hijo.puestos,padre)
     }
   }
 
 }
 
-const c_categorias =(hijo)=>{
+const c_categorias = (hijo,padre)=>{
   if(Array.isArray(hijo)){
     hijo.forEach(e=>{
       if(e.nombre){
-        c_cate+=1
-        console.log('**categoria**');
-        console.log('nombre pues:',e.nombre[0]);
+        let nombre = e.nombre[0]
+        //console.log('nombre pues:',e.nombre[0]);
+        let tempo = a_cate.find(el => el.nombre == nombre)
+        if(tempo === undefined){
+          a_cate.push({...e,padre:padre})
+        }
+
         if(e.categorias){
-          c_categorias(e.categorias)
+          c_categorias(e.categorias,padre)
         }
       }else{
-        c_categorias(e)
+        c_categorias(e,padre)
       }
     })
   }else{
     if(hijo.categoria){
-      c_categorias(hijo.categoria)
+      c_categorias(hijo.categoria,padre)
     }else{
-      c_categorias(hijo.categorias)
+      c_categorias(hijo.categorias,padre)
     }
   }
 }
 
-const c_requisitos =(hijo)=>{
+const c_requisitos =(hijo,padre)=>{
   if(Array.isArray(hijo)){
     hijo.forEach(e=>{
       if(e.nombre){
-        c_requi+=1
-        console.log('**requisito**');
-        console.log('nombre pues:',e.nombre[0]);
-        console.log('obligatorio pues:',e.obligatorio[0]);
+        let nombre = e.nombre[0]
+        let tamano = e.tamaño[0]
+        let obliga = e.obligatorio[0]
+        //console.log('nombre pues:',e.nombre[0]);
+        //console.log('tamaño pues:',e.tamaño[0]);
+        //console.log('obligatorio pues:',e.obligatorio[0]);
+        let tempo = a_requi.find(el => el.nombre == nombre)
+        if(tempo === undefined){
+          a_requi.push({...e,padre:padre})
+        }
+
         if(e.formatos){
-          c_formatos(e.formatos)
+          c_formatos(e.formatos,hijo)
         }
       }else{
-        c_requisitos(e.requisito)
+        c_requisitos(e.requisito,padre)
       }
     })
   }else{
     if(hijo.requisito){
-      c_requisitos(hijo.requisito)
+      c_requisitos(hijo.requisito,padre)
     }else{
-      c_requisitos(hijo.requisitos)
+      c_requisitos(hijo.requisitos,padre)
     }
   }
 }
 
-const c_formatos =(hijo)=>{
+const c_formatos =(hijo,padre)=>{
   if(Array.isArray(hijo)){
     hijo.forEach(e=>{
       if(e.nombre){
-        c_forma+=1
-        console.log('**formato**');
-        console.log('nombre pues:',e.nombre[0]);
+        let nombre = e.nombre[0]
+        //console.log('nombre pues:',e.nombre[0]);
+        let tempo = a_forma.find(el => el.nombre == nombre)
+        if(tempo === undefined){
+          a_forma.push({...e,padre:padre})
+        }
+
       }else{
-        c_formatos(e.formato)
+        c_formatos(e.formato,padre)
       }
     })
   }else{
     if(hijo.requisito){
-      c_formatos(hijo.requisito)
+      c_formatos(hijo.requisito,padre)
     }else{
-      c_formatos(hijo.requisitos)
+      c_formatos(hijo.requisitos,padre)
+    }
+  }
+}
+
+const query_select_insertar =  async(select,insert)=> {
+  let conn,retu;
+  try {
+    conn = await oracledb.getConnection(config_db);
+ 
+    retu = await conn.execute(select);
+    if(retu.rows.length === 0){
+      retu = await conn.execute(insert)
+    }
+    conn.commit()
+  } catch (err) {
+    console.log(err);
+  } finally {
+    if (conn) {
+      try {
+        await conn.close();
+      } catch (err) {
+        console.log('err');
+      }
+    }
+  }
+}
+
+const query_solo_insertar =  async(insertar)=> {
+  let conn,retu;
+  try {
+    conn = await oracledb.getConnection(config_db);
+ 
+    retu = await conn.execute(insertar);
+    conn.commit()
+  } catch (err) {
+    console.log(err);
+  } finally {
+    if (conn) {
+      try {
+        await conn.close();
+      } catch (err) {
+        console.log('err');
+      }
+      return retu
+    }
+  }
+}
+
+const conexion =  async(consulta)=> {
+  let conn,retu;
+  try {
+    conn = await oracledb.getConnection(config_db);
+ 
+    retu = await conn.execute(consulta);
+    conn.commit()
+  } catch (err) {
+    console.log(err);
+  } finally {
+    if (conn) {
+      try {
+        await conn.close();
+      } catch (err) {
+        console.log('err');
+      }
+      return retu
     }
   }
 }
